8cf5c0983f80427d00c2e2775995818e8b657cd7
0ashen <iusfilipp@gmail.com>
2021-01-08 21:33:28 +0300

    add initial config koa app

---
+++ server/app.ts
@@ -0,0 +1,7 @@
+const Koa = require('koa');
+
+const config = require('./lib/config');
+
+const app = new Koa();
+
+app.listen(config.port, () => console.log(`Server has been started on port ${config.port}`))
